a1efba09feb1cb1eacf9a684802b19575bf9e7a8
Szymon Pytel <szymon.pytel@gmail.com>
2021-05-01 17:34:55 +0000

    Hangman game

--- body.py
+++ body.py
@@ -1,1 +1,45 @@
+def parts(x):
+    if x == 0:
+        print('    ', '------')
+        print('    ', '|    |')
+        print('    ', '|     ')
+        print('    ', '|     ')
+        print('    ', '|     ')
+        print('    ', '|     ')
+        print('--------------')
 
+    elif x == 1:
+        print('    ', '------')
+        print('    ', '|    |')
+        print('    ', '|    O')
+        print('    ', '|     ')
+        print('    ', '|     ')
+        print('    ', '|     ')
+        print('--------------')
+
+    elif x == 2:
+        print('    ', '------')
+        print('    ', '|    |')
+        print('    ', '|    O')
+        print('    ', '|    |')
+        print('    ', '|     ')
+        print('    ', '|     ')
+        print('--------------')
+
+    elif x == 3:
+        print('    ', '------')
+        print('    ', '|    |')
+        print('    ', '|    O')
+        print('    ', '|   -|-')
+        print('    ', '|    |')
+        print('    ', '|     ')
+        print('--------------')
+
+    elif x == 4:
+        print('    ', '------')
+        print('    ', '|    |')
+        print('    ', '|    O')
+        print('    ', '|   -|-')
+        print('    ', '|    |')
+        print('    ', '|   / \\')
+        print('--------------')
--- hangman.py
+++ hangman.py
@@ -1,1 +1,52 @@
+from body import parts
+
+#set global variables
+secret_word = 'python'
+right = ['_'] * len(secret_word)
+wrong = []
+guess = ''
+flag = True
+
+
+def display_guess():
+    for i in right:
+        print(i, end = ' ')
+    print()
+
+display_guess()
+
+def hangman():
+    global flag
+    while flag:
+        print('=======================')
+        guess = input('Enter a letter ')
+
+        if guess in secret_word:
+
+            index = 0
+            for letter in secret_word:
+                if guess == letter:
+                    right[index] = guess
+                index += 1
+            display_guess()
+        else:
+            if guess in wrong:
+                print('Try again, you alread guessed that letter')
+            else:
+                if guess in wrong:
+                    print('Try again, you already guessed that letter')
+                else:
+                    wrong.append(guess)
+                    parts(len(wrong))
+                    print('Wrong:', wrong)
+
+        if '_' not in right:
+            print('You guessed that ! ')
+            flag = False
+        elif len(wrong) >= 4:
+            print('You lost')
+            break
+
+if __name__ == '__main__':
+    hangman()
 
